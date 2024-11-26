9c0bd1165c83c046623bfba03f2aba494cc661bb
Saulo Santos <166402067+seos-ink@users.noreply.github.com>
2024-11-26 13:16:23 -0300

    Re:Update 1.5

---
+++ _inc/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Header</title>
+    <link rel="stylesheet" href="css/mainstyle.css">
+</head>
+<body>
+    <nav class="navbar">
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="sobre.php">Sobre</a></li>
+        </ul>
+        <div class="logo">logo</div>
+        <div class="search-bar"></div>
+
+    </nav>
+</body>
+</html>
